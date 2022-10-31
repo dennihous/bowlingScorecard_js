7e1d904321859a201b47abdc6867dac6e038b2bd
dennis <denhouston1@icloud.com>
2022-10-31 21:51:25 +0000

    added practice and complete except final score frame

---
+++ aprac.js
@@ -0,0 +1,7 @@
+function bowlingScore(rolls){
+  if(rolls[rolls.length-2] === 10 || rolls[rolls.length-2]+rolls[rolls.length-1] === 10){
+    
+  }
+}
+
+console.log(bowlingScore([10,10,10,10,10,10,10,10,10,10,10,10]))
--- scoreCard.test.js
+++ scoreCard.test.js
@@ -1,20 +1,19 @@
-const { it } = require('node:test')
 const bowlingScore = require('./scoreCard')
 
 describe('calculates the score from a given game', () => {
   it('should be able to calculate score of only strikes', () => {
-    expect(bowlingScore([10,10,10,10,10,10,10,10,10,10,10,10])).toBe(300)
+    expect(bowlingScore([10,10,10,10,10,10,10,10,10,10,10,10])).toEqual(300)
   })
 
   it('should be able to calculate score of only 0s', () => {
-    expect(bowlingScore([0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0])).toBe(0)
+    expect(bowlingScore([0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0])).toEqual(0)
   })
 
   it('should be able to calcualte score of only spares', () => {
-    expect(bowlingScore([9,1, 9,1, 9,1, 9,1, 9,1, 9,1, 9,1, 9,1, 9,1, 9,1, 9])).toBe(190)
+    expect(bowlingScore([9,1, 9,1, 9,1, 9,1, 9,1, 9,1, 9,1, 9,1, 9,1, 9,1, 9])).toEqual(190)
   })
 
   it('should be able to calcualte score of only incomplete throws', () => {
-    expect(bowlingScore([8,1, 8,1, 8,1, 8,1, 8,1, 8,1, 8,1, 8,1, 8,1, 8,1])).toBe(90)
+    expect(bowlingScore([8,1, 8,1, 8,1, 8,1, 8,1, 8,1, 8,1, 8,1, 8,1, 8,1])).toEqual(90)
   })
 })
