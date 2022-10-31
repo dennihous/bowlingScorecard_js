5bb55520f3106c8051e7a7775670fe10f4079905
dennis <denhouston1@icloud.com>
2022-10-31 16:26:59 +0000

    added 3 tests

--- scoreCard.js
+++ scoreCard.js
@@ -2,4 +2,6 @@ function bowlingScore(rolls) {
   return 300
 }
 
+console.log(bowlingScore([9,1, 9,1, 9,1, 9,1, 9,1, 9,1, 9,1, 9,1, 9,1, 9,1, 9]))
+
 module.exports = bowlingScore
--- scoreCard.test.js
+++ scoreCard.test.js
@@ -1,7 +1,16 @@
+const { it } = require('node:test')
 const bowlingScore = require('./scoreCard')
 
 describe('calculates the score from a given game', () => {
   it('should be able to calculate score of only strikes', () => {
     expect(bowlingScore([10,10,10,10,10,10,10,10,10,10,10,10])).toBe(300)
+  })
+
+  it('should be able to calculate score of only 0s', () => {
+    expect(bowlingScore([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])).toBe(0)
+  })
+
+  it('should be able to calcualte score of only spares', () => {
+    expect(bowlingScore([9,1, 9,1, 9,1, 9,1, 9,1, 9,1, 9,1, 9,1, 9,1, 9,1, 9])).toBe(190)
   })
 })
