49b774e1ed5a029ebb58b810857900f9db325d77
dennis <denhouston1@icloud.com>
2022-10-31 17:31:05 +0000

    adding strike and splits to frames

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 1) If rolls[i] === 10 skip one
 
+2) If rolls[i] === 10 add rolls[i+1]+rolls[i+2]
+
+3) If rolls[i]+rolls[i+1] === 10 add rolls[i+2]
--- scoreCard.js
+++ scoreCard.js
@@ -1,8 +1,8 @@
 function bowlingScore(rolls) {
   let frames = []
-  for(let i = 0; i < rolls.length; rolls[i] === 10 ? i+= 3 :i+=2){
-    if(rolls[i] === 10){
-      frames.push(rolls[i])
+  for(let i = 0; i < rolls.length; rolls[i] === 10 ? i++ :i+=2){
+    if(rolls[i] === 10 || rolls[i]+rolls[i+1] === 10){
+      frames.push(rolls[i]+rolls[i+1]+rolls[i+2])
     } else {
       frames.push(rolls[i]+rolls[i+1])
     }
@@ -10,6 +10,6 @@ function bowlingScore(rolls) {
   return frames
 }
 
-console.log(bowlingScore([8,1, 8,1, 8,1, 8,1, 10, 8,1, 8,1, 8,1, 8,1, 8,1]))
+console.log(bowlingScore([8,1, 8,1, 8,1, 8,1, 10, 10, 8,1, 8,1, 8,1, 8,1]))
 
 module.exports = bowlingScore
